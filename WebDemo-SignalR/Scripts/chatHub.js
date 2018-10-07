0a50dfaf01501605dd00e737c8b6fe86f2ea0ede
Ke Yang <ke@feslen.com>
2018-10-07 19:37:15 +1300

    main code

---
+++ WebDemo-SignalR/Scripts/chatHub.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    var chatHub = $.connection.chatHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    chatHub.client.newPost = function (post) {
+        chatViewModel.addPost(post);
+    };
+
+    var ChatViewModel = function () {
+        var self = this;
+        self.username = "";
+        self.post = ko.observable("");
+        self.postList = ko.observableArray();
+    };
+
+    ChatViewModel.prototype = {
+        sendPost: function () {
+            var self = this;
+            chatHub.server.chat(self.username, self.post());
+            self.post("");
+        },
+        addPost: function (post) {
+            var self = this;
+            self.postList.push(post);
+        }
+    }
+    var chatViewModel = new ChatViewModel();
+
+    $(function () {
+        ko.applyBindings(chatViewModel);
+    });
+}());
---
+++ WebDemo-SignalR/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDemo_SignalR.Startup))]
+
+namespace WebDemo_SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
